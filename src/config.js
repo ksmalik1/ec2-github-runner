378dc2fb1f3179d915430966a331e996073d9ad2
Sage Rimal <srimal@campusexplorer.com>
2022-07-11 17:09:03 -0700

    comment this one

--- src/config.js
+++ src/config.js
@@ -13,8 +13,8 @@ class Config {
       label: core.getInput('label'),
       ec2InstanceId: core.getInput('ec2-instance-id'),
       iamRoleName: core.getInput('iam-role-name'),
-      runnerHomeDir: core.getInput('runner-home-dir'),
-      otherCreateInstancesParams: JSON.parse(core.getInput('other-createInstances-params'))
+      runnerHomeDir: core.getInput('runner-home-dir')
+      //otherCreateInstancesParams: JSON.parse(core.getInput('other-createInstances-params'))
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
